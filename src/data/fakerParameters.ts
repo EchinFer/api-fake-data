a371f70a1356c9612bbee260a07a37250f6372b4
FernandoAlfonso <echinfer@gmail.com>
2023-12-12 16:45:51 -0300

    Refactor code to improve performance and readability

--- src/data/fakerParameters.ts
+++ src/data/fakerParameters.ts
@@ -9,23 +9,475 @@ import { faker } from '@faker-js/faker';
 //     valorHasta: string;
 //     listaValores: string;
 //     idGrupo: string;
+
+// [
+//     {
+//       "idParametros": "1",
+//       "nombreParametro": "Porcentaje de Endeudamiento.",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "40",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "2",
+//       "nombreParametro": "Monto cobro mínimo mensual.",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "1500000",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "3",
+//       "nombreParametro": "Cantidad de Préstamos",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "1",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "4",
+//       "nombreParametro": "Referencia de Atraso",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": ",SIN DATOS,BUENO,MUY BUENO,EXCELENTE,",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "5",
+//       "nombreParametro": "Cliente Físico",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "F",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "6",
+//       "nombreParametro": "Posee cuenta en Guaraníes?",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "SI",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "7",
+//       "nombreParametro": "Rango de Edad",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "R",
+//       "valorDesde": "19",
+//       "valorHasta": "65",
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "8",
+//       "nombreParametro": "Calificación del Banco",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": ",0,1,",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "9",
+//       "nombreParametro": "Calificación del BCP",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": ",0,1,",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "10",
+//       "nombreParametro": "Es Codeudor?",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "NO",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "11",
+//       "nombreParametro": "Antiguedad Laboral",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "6",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "12",
+//       "nombreParametro": "Mora",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "NO",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "13",
+//       "nombreParametro": "Meses para calculo de Promedio.",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "3",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "14",
+//       "nombreParametro": "Conceptos a tener en cuenta para el promedio.",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": ",COMISIONES,VIATICOS,AGUINALDO,INDEMNIZACION,DEMAS CONCEPTOS,",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "15",
+//       "nombreParametro": "Coeficiente Gastos Administrativos",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "5",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "16",
+//       "nombreParametro": "Coeficiente Seguro",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "1,188",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "17",
+//       "nombreParametro": "Listas Negativas.",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "OFAC = NO ,ALD = NO ,ONU = NO",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "18",
+//       "nombreParametro": "Sucursales Excluidas",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": ",988-casamatriz,",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "19",
+//       "nombreParametro": "Campaña PraAprobados PGS",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "84",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "20",
+//       "nombreParametro": "ROS",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "NO",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     },
+//     {
+//       "idParametros": "21",
+//       "nombreParametro": "Es pago Salario?",
+//       "tipoDato": "string",
+//       "estado": "APROBADO",
+//       "tipoValor": "U",
+//       "valorDesde": "SI",
+//       "valorHasta": null,
+//       "listaValores": null,
+//       "idGrupo": "2"
+//     }
+//   ]
 const generateData = (quantity: number) => {
 
-    const data: ParameterDto[] = [];
-
-    for (let i = 0; i < quantity; i++) {
-        data.push({
-            idParametros: faker.number.bigInt().toString(),
-            nombreParametro: faker.lorem.words(5),
-            tipoDato: faker.database.type(),
-            estado: faker.finance.currencyCode(),
-            tipoValor: faker.database.column(),
-            valorDesde: faker.number.int().toString(),
-            valorHasta: faker.number.int().toString(),
-            listaValores: faker.database.column(),
-            idGrupo: faker.number.bigInt().toString(),
-        });
-    }
+    const data: ParameterDto[] = [
+        {
+            idParametros: "1",
+            nombreParametro: "Porcentaje de Endeudamiento.",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "40",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "2",
+            nombreParametro: "Monto cobro mínimo mensual.",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "1500000",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "3",
+            nombreParametro: "Cantidad de Préstamos",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "1",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "4",
+            nombreParametro: "Referencia de Atraso",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: ",SIN DATOS,BUENO,MUY BUENO,EXCELENTE,",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "5",
+            nombreParametro: "Cliente Físico",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "F",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "6",
+            nombreParametro: "Posee cuenta en Guaraníes?",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "SI",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "7",
+            nombreParametro: "Rango de Edad",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "R",
+            valorDesde: "19",
+            valorHasta: "65",
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "8",
+            nombreParametro: "Calificación del Banco",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: ",0,1,",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "9",
+            nombreParametro: "Calificación del BCP",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: ",0,1,",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "10",
+            nombreParametro: "Es Codeudor?",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "NO",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "11",
+            nombreParametro: "Antiguedad Laboral",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "6",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "12",
+            nombreParametro: "Mora",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "NO",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "13",
+            nombreParametro: "Meses para calculo de Promedio.",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "3",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "14",
+            nombreParametro: "Conceptos a tener en cuenta para el promedio.",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: ",COMISIONES,VIATICOS,AGUINALDO,INDEMNIZACION,DEMAS CONCEPTOS,",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "15",
+            nombreParametro: "Coeficiente Gastos Administrativos",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "5",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "16",
+            nombreParametro: "Coeficiente Seguro",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "1,188",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "17",
+            nombreParametro: "Listas Negativas.",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "OFAC = NO ,ALD = NO ,ONU = NO",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "18",
+            nombreParametro: "Sucursales Excluidas",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: ",988-casamatriz,",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "19",
+            nombreParametro: "Campaña PraAprobados PGS",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "84",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "20",
+            nombreParametro: "ROS",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "NO",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        },
+        {
+            idParametros: "21",
+            nombreParametro: "Es pago Salario?",
+            tipoDato: "string",
+            estado: "APROBADO",
+            tipoValor: "U",
+            valorDesde: "SI",
+            valorHasta: null,
+            listaValores: null,
+            idGrupo: "2"
+        }
+    ];
 
     return data;
 }
